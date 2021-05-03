4af6faebff070858320ea5f9e56745a40e4b0033
victor kigen <vickigen@gmail.com>
2021-05-03 11:52:13 +0300

    add defined config files

--- app/__init__.py
+++ app/__init__.py
@@ -18,7 +18,7 @@ photos = UploadSet('photos',IMAGES)
 
 def create_app():
     app = Flask(__name__)
-    app.config.from_object(Config)
+    app.config.from_object(config_options[config_name])
     from .auth import auth as authentication_blueprint
     from .main import main as main_blueprint
 
